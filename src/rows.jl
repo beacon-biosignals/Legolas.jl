ecc0143e7501b8817c5b01cb34ac5ed23ce98eb6
Jarrett Revels <jarrettrevels@gmail.com>
2022-04-21 08:33:48 -0400

    Update src/rows.jl

--- src/rows.jl
+++ src/rows.jl
@@ -119,7 +119,7 @@ function Base.showerror(io::IO, e::UnknownSchemaError)
               your environment is as expected).
 
               Note that if you're in this particular situation, you can still load
-              the raw table as-is. For example, to load an Arrow table, call `Arrow.Table(path)`.
+              the raw table as-is without Legolas; e.g., to load an Arrow table, call `Arrow.Table(path)`.
               """)
     return nothing
 end
