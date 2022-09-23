803387205b2eea35987784b844e24c9d8172a0eb
Jarrett Revels <jarrettrevels@gmail.com>
2022-09-23 17:17:32 -0400

    tmp

--- src/Legolas.jl
+++ src/Legolas.jl
@@ -8,10 +8,26 @@ include("lift.jl")
 include("schemas.jl")
 include("tables.jl")
 
-# TODO: deprecations for:
-#
-# - `Legolas.Row`` + `Legolas.@row`
-# - relevant methods against `Type{<:Schema}` instead of `Schema`
-#
+#=
+CHANGELOG
+
+- rm'd `Legolas.Row` in favor of `Legolas.row`
+- rm'd methods against `::Type{<:Schema}` in favor of `::Schema`
+- `Legolas.@row` -> `Legolas.@schema`
+- better introspection/validation tools (`schema_fields`, `schema_field_statements`, compliance predicate)
+- better enforcement of expected properties:
+    - no duplicate fields in schema definitions
+    - https://github.com/beacon-biosignals/Legolas.jl/issues/51
+    - https://github.com/beacon-biosignals/Legolas.jl/issues/53
+- otherwise resolves/obviates:
+    - https://github.com/beacon-biosignals/Legolas.jl/issues/49
+    - https://github.com/beacon-biosignals/Legolas.jl/issues/50
+
+TODO: deprecations for:
+
+- `Legolas.Row`
+- `Legolas.@row`
+- relevant methods against `Type{<:Schema}` instead of `Schema`
+=#
 
 end # module
