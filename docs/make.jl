4d49630409ed1f1a5851f8f89533553c79115de8
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2022-11-22 18:35:28 +0100

    start upgrade guide, and add pieces from LegolasFlux upgrade (#71)
    * start upgrade guide, and add pieces from LegolasFlux upgrade
    
    * Update docs/src/upgrade.md
    
    * Update docs/src/upgrade.md
    
    Co-authored-by: Eric Hanson <5846501+ericphanson@users.noreply.github.com>
    
    * Update docs/src/upgrade.md
    
    Co-authored-by: Jarrett Revels <jarrettrevels@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Jarrett Revels <jarrettrevels@gmail.com>
    
    * Update docs/src/upgrade.md
    
    Co-authored-by: Jarrett Revels <jarrettrevels@gmail.com>

--- docs/make.jl
+++ docs/make.jl
@@ -7,7 +7,8 @@ makedocs(modules=[Legolas],
          pages=["API Documentation" => "index.md",
                 "Schema-Related Concepts/Conventions" => "schema-concepts.md",
                 "Arrow-Related Concepts/Conventions" => "arrow-concepts.md",
-                "FAQ" => "faq.md"])
+                "FAQ" => "faq.md",
+                "Upgrading from v0.4 to v0.5" => "upgrade.md"])
 
 deploydocs(repo="github.com/beacon-biosignals/Legolas.jl.git",
            push_preview=true,
