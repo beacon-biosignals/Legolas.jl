af3249b51cddf5bf2f95da85d1fddea378aeca92
Curtis Vogt <curtis.vogt@gmail.com>
2023-12-19 11:16:49 -0600

    Rename to `record_merge`

--- src/record_merge.jl
+++ src/record_merge.jl
@@ -1,12 +1,12 @@
 """
-    recordmerge(record; fields_to_merge...)
+    record_merge(record; fields_to_merge...)
 
 Return a new `AbstractRecord` with the same schema as `record` by merging the fields of
 `record` and `fields_to_merge` together using `Tables.rowmerge`.
 
 See also: `Tables.rowmerge`.
 """
-function recordmerge(record::AbstractRecord; fields_to_merge...)
+function record_merge(record::AbstractRecord; fields_to_merge...)
     # Avoid using `typeof(record)` as can cause constructor failures with parameterized 
     # record types.
     R = record_type(schema_version_from_record(record))
