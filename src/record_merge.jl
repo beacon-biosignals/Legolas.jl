4c40fc78b790fcdad776412b65f42763d16dc12d
Jarrett Revels <jarrettrevels@gmail.com>
2023-12-21 10:30:46 -0500

    Update src/record_merge.jl

--- src/record_merge.jl
+++ src/record_merge.jl
@@ -1,8 +1,10 @@
 """
-    record_merge(record; fields_to_merge...)
+    record_merge(record::AbstractRecord; fields_to_merge...)
 
-Return a new `AbstractRecord` with the same schema as `record` by merging the fields of
-`record` and `fields_to_merge` together using `Tables.rowmerge`.
+Return a new `AbstractRecord` with the same schema version as `record`, whose fields
+are computed via `Tables.rowmerge(record; fields_to_merge...)`. The returned record is 
+constructed by passing these merged fields to the `AbstractRecord` constructor 
+that matches the type of the input `record`.
 """
 function record_merge(record::AbstractRecord; fields_to_merge...)
     # Avoid using `typeof(record)` as can cause constructor failures with parameterized 
