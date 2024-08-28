37971d817115612c16ad823ce7b6e7db079ac82c
Curtis Vogt <curtis.vogt@gmail.com>
2024-08-28 15:44:06 -0500

    Require ConstructionBase 1.5.7

--- ext/LegolasConstructionBaseExt.jl
+++ ext/LegolasConstructionBaseExt.jl
@@ -12,21 +12,11 @@ using Legolas: AbstractRecord
 if VERSION < v"1.7"
     ConstructionBase.getproperties(r::AbstractRecord) = NamedTuple(r)
 
+    # This is largely copy-paste from `ConstructionBase.setproperties_object` (v1.5.7):
+    # https://github.com/JuliaObjects/ConstructionBase.jl/blob/71fb5a5198f41f3ef29a53c01940cf7cf6b233eb/src/ConstructionBase.jl#L205-L209
     function ConstructionBase.setproperties(r::R, patch::NamedTuple) where {R<:AbstractRecord}
-        if isdefined(ConstructionBase, :check_patch_properties_exist)
-            # This is largely copy-paste from `ConstructionBase.setproperties_object`:
-            # https://github.com/JuliaObjects/ConstructionBase.jl/blob/cd24e541fd90ab54d2ee12ddd6ccd229be9a5f1e/src/ConstructionBase.jl#L211-L218
-            nt = getproperties(r)
-            nt_new = merge(nt, patch)
-            ConstructionBase.check_patch_properties_exist(nt_new, nt, r, patch)
-            args = Tuple(nt_new)  # old Julia inference prefers if we wrap in `Tuple`
-            return constructorof(R)(args...)
-        else
-            # As of ConstructionBase 1.5.7 the internals of `ConstructionBase.setproperties_object` have changed:
-            # https://github.com/JuliaObjects/ConstructionBase.jl/blob/71fb5a5198f41f3ef29a53c01940cf7cf6b233eb/src/ConstructionBase.jl#L205-L209
-            ConstructionBase.check_patch_fields_exist(r, patch)
-            return ConstructionBase.setfields_object(r, patch)
-        end
+        ConstructionBase.check_patch_fields_exist(r, patch)
+        return ConstructionBase.setfields_object(r, patch)
     end
 end
 
