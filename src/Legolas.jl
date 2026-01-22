609851e17d776c11996e10f2be4c078255d1b278
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2026-01-22 13:32:36 +0100

    Declare public API

--- src/Legolas.jl
+++ src/Legolas.jl
@@ -2,6 +2,18 @@ module Legolas
 
 using Tables, Arrow, UUIDs
 
+# public API
+if VERSION >= v"1.11.0-DEV.469"
+    eval(Meta.parse("""
+    public SchemaVersion, @schema, @version, @check, is_valid_schema_name,
+           parse_identifier, name, version, identifier, schema_provider, parent,
+           declared_fields, declaration, record_type, schema_version_from_record,
+           declared, find_violation, find_violations, complies_with, validate,
+           accepted_field_type, extract_schema_version, write, read, tobuffer,
+           lift, construct, record_merge, gather, locations, materialize
+    """))
+end
+
 const LEGOLAS_SCHEMA_QUALIFIED_METADATA_KEY = "legolas_schema_qualified"
 const LEGOLAS_SCHEMA_PROVIDER_NAME_METADATA_KEY = "legolas_julia_schema_provider_name"
 const LEGOLAS_SCHEMA_PROVIDER_VERSION_METADATA_KEY = "legolas_julia_schema_provider_version"
